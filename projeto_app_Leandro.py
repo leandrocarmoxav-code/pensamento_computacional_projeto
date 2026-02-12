d76c4595be5203e81c782d59a78b957633b0a513
Leandro do carmo en vocaçao <leandrocarmoxav@gmail.com>
2026-02-12 15:19:44 -0300

    Update, Barbearia
    crie o meu primeiro app pyton colocando um menu de uma barbearia e outros sistemas

---
+++ projeto_app_Leandro.py
@@ -0,0 +1,64 @@
+'''
+crud
+
+Barbearia
+
+Estou no sistema e clico em “Novo Cliente”.
+
+Preencho:
+
+Nome: Carlos Souza
+
+Telefone: (11) 98888-7777
+
+Serviço: Corte + Barba
+
+Clico em Salvar.
+
+
+Carlos volta depois de um mês.
+
+Eu vou na busca do sistema e digito: Carlos Souza.
+
+O sistema mostra:
+
+Último serviço: Corte + Barba
+
+Data: 10/01/2026
+
+Telefone: (11) 98888-7777
+
+
+Carlos fala:
+“Agora quero fazer só corte.”
+
+Eu entro no cadastro dele, altero:
+
+Serviço preferido: Apenas Corte
+
+Clico em Atualizar.
+
+Percebo que existe um cadastro duplicado do Carlos.
+
+Seleciono o cadastro errado e clico em Excluir.
+
+🗑️ Confirmo a exclusão.
+
+Pronto, removi o registro duplicado.
+'''
+
+
+print("\n=== Sistema da Barbearia ===")
+print("1. Criar Conta")
+print("2. Agendar Cliente")
+print("3. Ver Agenda")
+print("4. pacote de assinatura")
+print("5. Mudar Serviço")
+print("6. Cancelar Agendamento")
+print("7. Sair")
+
+print(" acessando usúario")
+print(" agendar serviço")
+print(" visualizar pacotes de assinatura")
+print(" reagendar horario")
+    
