fd0c6c323761ec7e88f7b8da4458eb7f64006d9b
Leandro do carmo en vocaçao <leandrocarmoxav@gmail.com>
2026-03-09 16:57:09 -0300

    update - Atividade
    1- Teste de idade,
    2- Projeto do salão
    3- Aprender a simplificação do if,elif e o else

---
+++ projeto_app_leandronovo.py
@@ -0,0 +1,79 @@
+'''
+CRUD
+
+
+
+'''
+
+
+
+
+print("\n== GESTÃO DO SALÃO ===")
+print("1. Agendar Cliente")
+print("2. Ver Agenda")
+print("3. Mudar Serviço")
+print("4. Cancelar Agendamento")
+print("5. Relatorio de Serviços")
+print("0. Sair")
+
+while True:
+
+    escolha = input("\nEscolha uma opção: ")
+
+    if escolha == '1':
+
+        print("Cadastar cliente...")
+        nome_cliente = input("Digite o nome do Cliente: ")
+        cliente_telefone = input("Digite o telefone do cliente: ")
+        
+        print("Cadastro concluido com sucesso\n")
+
+    elif escolha == '2':
+
+        print("mostrar os horarios cadastrados")
+        horario_cliente = input("Escolha um horario: ")
+        periodo_cliente = input("Escolha o periodo: ")
+
+        print("Horario Cadastrado com sucesso\n")
+
+    if escolha == '3':
+
+        print("Alterar serviço do cliente")
+        servico_cliente = input(" digite o novo serviço (corte / barba / Corte + Barba): ")
+        print("Serviço alterado com sucesso!")
+
+    elif escolha == '4':
+
+        print("Cancelar Agendamento")
+        cliente = input("Digite o nome do cliente que deseja cancelar: ")
+        horario = input("Digite o horário do agendamento:")
+        print(" Cancelamento com sucesso")
+
+
+
+
+
+
+
+    
+
+
+
+        
+    
+
+
+ 
+
+     
+
+      
+       
+    
+        
+    
+
+   
+    
+
+
---
+++ teste_idade.py
@@ -0,0 +1,17 @@
+'''
+
+
+'''
+
+print('Digite a idade do jovem: ')
+
+idade = int(input())
+
+if idade < 18:
+    print('Jovem e menor de idade')
+
+elif idade <=65:
+    print('jovem e adulto, é maior de idade')
+
+else:
+    print('Jovem é idoso de idade')
